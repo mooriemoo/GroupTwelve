d4e6b3ac9cd1d6c4e795002493a54a2b076f77d0
mooriemoo <mariia19190@gmail.com>
2019-04-11 16:33:08 -0700

    Resolved conflict

--- GroupTwelve/src/Main.java
+++ GroupTwelve/src/Main.java
@@ -4,12 +4,10 @@ public class Main {
 	public static void main(String[] args)
 	{
 		System.out.print("Hello World");
-<<<<<<< HEAD
 		
 		//Mariia Kostyleva
-=======
 		//Alexander Perrotta 300245277
->>>>>>> branch 'master' of https://github.com/mooriemoo/GroupTwelve.git
+
 	}
 
 }
